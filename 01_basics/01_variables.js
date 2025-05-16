623292bd44f96e92d8d35d51e782ff0b6afab365
Nitant Sharma <nitantsharma292@gmail.com>
2025-05-16 06:29:44 +0000

    variables notes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 145434
+let accountEmail = "abc@google.com"
+var accountPassword = "5455554"
+accountCity = "Daman"
+let accountState;
+
+// accountId = 1120114 // Not Allowed
+
+accountEmail = "nitant@google.com"
+accountPassword = "67676767"
+accountCity = "Gurugram"
+
+/* Prefer not to use var
+because of issue in block scope and functional scope
+ */
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
