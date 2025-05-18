cb2f46995966d815ba29255f8499a251af18cf12
Nitant Sharma <nitantsharma292@gmail.com>
2025-05-18 08:32:52 +0000

    oops notes

---
+++ 10_oops/oop.js
@@ -0,0 +1,38 @@
+// Object Literal
+const user = {
+    username: "Nitant",
+    loginCount: 8,
+    signedIn: true,
+
+    getUserDetails: function(){
+        //console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+
+}
+
+
+
+//console.log(user.username)
+//console.log(user.getUserDetails());
+// console.log(this);
+
+//Constructor Function
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn
+
+    this.greeting = function(){
+        console.log(`Welcome ${this.username}`);
+
+    }
+
+    return this
+}
+
+const userOne = new User("Nitant", 12, true)
+const userTwo = new User("OOPS in JS", 11, false)
+console.log(userOne.constructor);
+//console.log(userTwo);
